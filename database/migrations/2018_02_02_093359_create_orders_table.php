671bfb4c265401cbc35f823292ce6fce2f915b76
Winnerawan T <winnerawan@ACA8016C.ipt.aol.com>
2018-02-02 16:49:00 +0700

    add Order & Order Detail Model

---
+++ database/migrations/2018_02_02_093359_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('restaurant_id');
+            $table->integer('customer_id');
+            $table->decimal('sub_total', 8, 2);
+            $table->decimal('tax', 8, 2);
+            $table->decimal('total_with_tax', 8, 2);
+            $table->string('voucher_code')->nullable();
+            $table->decimal('voucher_ammount', 8, 2)->nullable();
+            $table->string('delivery_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
