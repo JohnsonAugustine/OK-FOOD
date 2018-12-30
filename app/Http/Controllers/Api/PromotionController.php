4cda12c280eb33f7b1feeca1362879c2cd0730a9
Winnerawan T <third1337@gmail.com>
2018-12-30 14:32:07 +0700

    update promo

--- app/Http/Controllers/Api/PromotionController.php
+++ app/Http/Controllers/Api/PromotionController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use JWTAuth;
 use Response;
+use URL;
 use App\Repository\Transformers\MenuTransformer;
 use \Illuminate\Http\Response as Res;
 use Validator;
@@ -21,6 +22,10 @@ class PromotionController extends ApiController
     public function promos() {
 
         $promotion = Promotion::all();
+
+        foreach($promotion as $promo) {
+            $promo->image = URL::asset('images/'.$promo->image);
+        }
 
         return $this->respond([
 
